03fcb6d19380749fcb18d1827cdd76c29c34a7a8
brian <briangzimmerman@gmail.com>
2019-04-19 20:59:19 -0500

    Working on Layout

--- app/src/main/java/com/example/wifiringtone/MainActivity.java
+++ app/src/main/java/com/example/wifiringtone/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.wifiringtone;
 
 
+import android.app.ActionBar;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,13 +29,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        populateSaved();
     }
 
     protected void populateSaved() {
         Map<String, ?> saved = this.ringtone_settings.getAll();
+        LinearLayout container = ((LinearLayout) findViewById(R.id.saved_settings));
 
         for(String wifi_network : saved.keySet()) {
+            CardView card = new CardView(getApplicationContext());
 
+            CardView.LayoutParams params = new CardView.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    100
+            );
+
+            card.setLayoutParams(params);
+            card.setRadius(5);
+            card.setCardBackgroundColor(Color.parseColor("#FFC6D6C3"));
+
+            container.addView(card);
         }
     }
 }
