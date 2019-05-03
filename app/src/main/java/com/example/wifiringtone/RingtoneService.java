b7f00daa1c9d65fc229ad05c3dba5b38ea79d868
brian <briangzimmerman@gmail.com>
2019-05-03 11:31:48 -0500

    Working on Layout

--- app/src/main/java/com/example/wifiringtone/RingtoneService.java
+++ app/src/main/java/com/example/wifiringtone/RingtoneService.java
@@ -1,63 +1,33 @@
 package com.example.wifiringtone;
 
-import android.app.Notification;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.os.IBinder;
-import android.util.Log;
 
 public class RingtoneService extends Service {
-    private static final int NOTIF_ID = 1;
-    private static final String NOTIF_CHANNEL_ID = "ringtone_channel_id";
-    private NotificationManager nm;
+    private WifiBroadcastReceiver wbr;
 
     @Override
     public void onCreate() {
-        this.nm = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-
-        // Display a notification about us starting.
-        showNotification();
+        this.wbr = new WifiBroadcastReceiver(this, (NotificationManager)getSystemService(NOTIFICATION_SERVICE));
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        showNotification();
+        this.wbr.showNotification();
 
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
-        this.nm.cancel(this.NOTIF_ID);
+        this.wbr.destroyNotification();
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    public void showNotification() {
-        //Open the app when notification is clicked
-        PendingIntent appIntent = PendingIntent.getActivity(this, 0,
-                new Intent(this, MainActivity.class), 0);
-
-//        Uri defaultRingtoneUri = RingtoneManager.getActualDefaultRingtoneUri(RingtoneManager.TYPE_RINGTONE);
-//        Ringtone defaultRingtone = RingtoneManager.getRingtone(getActivity(), defaultRingtoneUri);
-
-        Notification notification = new Notification.Builder(this)
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setOngoing(true)
-                .setContentTitle(getString(R.string.app_name))
-                .setContentText("Current Ringtone: ")
-                .setContentIntent(appIntent)
-                .build();
-
-        this.nm.notify(this.NOTIF_ID, notification);
     }
 }
--- app/src/main/java/com/example/wifiringtone/WifiBroadcastReceiver.java
+++ app/src/main/java/com/example/wifiringtone/WifiBroadcastReceiver.java
@@ -1,24 +1,53 @@
 package com.example.wifiringtone;
 
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.net.wifi.SupplicantState;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.util.Log;
 
+import static android.content.Context.NOTIFICATION_SERVICE;
+
 public class WifiBroadcastReceiver extends BroadcastReceiver {
+    private static final int NOTIF_ID = 1;
+    private static final String NOTIF_CHANNEL_ID = "ringtone_channel_id";
     private final String PREF_NAME = "WIFI_RINGTONE_PREFS";
     private SharedPreferences ringtone_settings;
+    private NotificationManager nm;
     private Context context;
-    private String current_ringtone;
+    private String current_ringtone = "";
 
-    public WifiBroadcastReceiver(Context context) {
+    public WifiBroadcastReceiver(Context context, NotificationManager nm) {
         this.context = context;
+        this.nm = nm;
         this.ringtone_settings = context.getSharedPreferences(this.PREF_NAME, 0);
+
+        NotificationChannel nc = new NotificationChannel(this.NOTIF_CHANNEL_ID, "ringtone_channel", nm.IMPORTANCE_HIGH);
+        nc.setDescription("Current Ringtone");
+        nc.enableLights(false);
+        nc.enableVibration(false);
+        nc.setShowBadge(false);
+
+        this.nm.createNotificationChannel(nc);
+
+        Uri defaultRingtoneUri = RingtoneManager.getActualDefaultRingtoneUri(this.context, RingtoneManager.TYPE_RINGTONE);
+        Ringtone defaultRingtone = RingtoneManager.getRingtone(this.context, defaultRingtoneUri);
+        this.current_ringtone = defaultRingtone.getTitle(this.context);
+
+        Log.d("Current Ringtone:", this.current_ringtone);
+
+        showNotification();
     }
 
     @Override
@@ -49,5 +78,25 @@ public class WifiBroadcastReceiver extends BroadcastReceiver {
         }
 
         return ringtone;
+    }
+
+    public void destroyNotification() {
+        this.nm.cancel(this.NOTIF_ID);
+    }
+
+    public void showNotification() {
+        //Open the app when notification is clicked
+        PendingIntent appIntent = PendingIntent.getActivity(this.context, 0,
+                new Intent(this.context, MainActivity.class), 0);
+
+        Notification notification = new Notification.Builder(this.context, this.NOTIF_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setOngoing(true)
+                .setContentTitle(this.context.getString(R.string.app_name))
+                .setContentText("Current Ringtone: " + this.current_ringtone)
+                .setContentIntent(appIntent)
+                .build();
+
+        this.nm.notify(this.NOTIF_ID, notification);
     }
 }
